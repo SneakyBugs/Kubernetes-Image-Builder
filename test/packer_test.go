45810527fcd2c588dff5cdf9ca01c829c148506e
Lior Friedman <lior@sneakybugs.com>
2024-09-01 08:36:01 +0000

    test: add cluster initialization test
    Add cluster initialization and basic validation to the test. The test
    now clones a VM from the image, runs kubeadm init, and connects to the
    cluster with kubectl and waits for pods to become ready.
    Fix issue with testing Packer v1.10 and above by upgrading Terratest.

--- test/packer_test.go
+++ test/packer_test.go
@@ -2,12 +2,16 @@ package test
 
 import (
 	"os"
+	"strings"
 	"testing"
+	"time"
 
+	"github.com/gruntwork-io/terratest/modules/k8s"
 	"github.com/gruntwork-io/terratest/modules/packer"
 	"github.com/gruntwork-io/terratest/modules/shell"
 	"github.com/gruntwork-io/terratest/modules/ssh"
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestPackerImage(t *testing.T) {
@@ -46,4 +50,35 @@ func TestPackerImage(t *testing.T) {
 
 	// Check Cloud Init ran successfully and SSH works.
 	ssh.CheckSshCommand(t, host, "cloud-init status --wait")
+
+	containerdShowState := ssh.CheckSshCommand(t, host, "sudo systemctl show containerd --property=ActiveState")
+	if !strings.Contains(containerdShowState, "ActiveState=active") {
+		t.Fatalf("Expected `systemctl show containerd` output to contain `ActiveState=active`, got: `%s`\n", containerdShowState)
+	}
+
+	ssh.CheckSshCommand(t, host, "sudo kubeadm init")
+	kubeconfigContent := ssh.FetchContentsOfFile(t, host, true, "/etc/kubernetes/admin.conf")
+	kubeconfigFile, err := os.CreateTemp("", "kubeconfig")
+	if err != nil {
+		t.Fatalf("Expected no error, got %v\n", err)
+	}
+	_, err = kubeconfigFile.WriteString(kubeconfigContent)
+	if err != nil {
+		t.Fatalf("Expected no error, got %v\n", err)
+	}
+	kubeconfig := kubeconfigFile.Name()
+	kubeconfigFile.Close()
+
+	kubectlOptions := k8s.NewKubectlOptions("kubernetes-admin@kubernetes", kubeconfig, "kube-system")
+
+	k8s.WaitUntilNumPodsCreated(t, kubectlOptions, v1.ListOptions{}, 7, 10, time.Second*5)
+
+	pods := k8s.ListPods(t, kubectlOptions, v1.ListOptions{})
+	for _, pod := range pods {
+		if strings.Contains(pod.Name, "coredns") {
+			// CoreDNS pods should have "not ready" status because there is no network plugin installed.
+			continue
+		}
+		k8s.WaitUntilPodAvailable(t, kubectlOptions, pod.Name, 10, time.Second*10)
+	}
 }
